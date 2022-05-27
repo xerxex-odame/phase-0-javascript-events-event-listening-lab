2b158b56537e2eb88a9e76fce2e7a95e7d96697d
xerxex-odame <kingodame@yahoo.com>
2022-05-27 02:17:10 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-function addingEventListener() {
-}
+// Your code here
+document.addEventListener("keydown", function(event){
+    if(event.key === 'ArrowRight'){
+      moveDodgerRight();
+    };
+    if(event.key === 'ArrowLeft'){
+      moveDodgerLeft();
+    };
+  
+  });
+  
+  const dodger = document.getElementById("dodger");
+  
+  function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+    if(left > 0){
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px","");
+    const right = parseInt(rightNumbers, 10);
+    if(right > 0){
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
+  
